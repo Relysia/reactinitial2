325d23f641975043dd88d92ce74ed556350ec0e1
Relysia <baranyairiki@gmail.com>
2021-04-27 16:54:25 +0200

    Fetch data and basic setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Client from './components/Client.js';
 
 const App = () => {
+  const [clients, setClients] = useState(null);
+  const [searchWord, setSearchWord] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`api/clients?search=${searchWord}`);
+      const result = await response.json();
+      setClients(result);
+    } catch (error) {
+      console.log('Failed to get data from backend');
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(clients);
 
   return (
-    <div className="App">
-      Todo...
+    <div className='App'>
+      <h1>Veterinarian admin - clients</h1>
+      <input type='text' value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
+      <button>Search</button>
+      {clients && clients.map((client) => <Client client={client} />)}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Client.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Client({ client }) {
+  return (
+    <div>
+      <h2>{client.name}</h2>
+    </div>
+  );
+}
+
+export default Client;
